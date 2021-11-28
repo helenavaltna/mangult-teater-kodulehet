f6c98956a59655a827a8a7039fe67a3401cd306f
tundrajänes <34316477+mahlane-arbuus@users.noreply.github.com>
2021-11-28 12:53:38 +0200

    lisasin kodulehele javasriptiga mapsi ja mõned uuendused juurde

--- new 1.js
+++ new 1.js
@@ -1,1 +1,13 @@
-//siia tuleb javascripti kood
+function initMap() {
+		//panin asukohaks Delta keskuse
+        const asukoht = { lat: 58.385, lng: 26.725};
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 15,
+          center: asukoht,
+        });
+		//punase markeri lisamine kaardile
+        const marker = new google.maps.Marker({
+          position: asukoht,
+          map: map,
+        });
+      }
