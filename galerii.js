2a1a7f670d805524d47e920211d5ac4608fde887
ReenaSeeba <reena.seeba@gmail.com>
2021-11-29 21:07:20 +0200

    galerii failide kommenteerimine ja allikale viitamine ning ésilehe korrastamine.

--- galerii.js
+++ galerii.js
@@ -1,4 +1,10 @@
+// Järgnev Javascript kood pärineb tervenisti siit: https://codepen.io/Snip3r/pen/GxybbN
+// Samast kohast pärineb ka sellega kokku kuuluv galerii.css faili sisu ja slaideriga
+// seotud html-koodi elemendid. See Javascript aitab teostada veebilehel kuvatavat piltide
+// animeeritud slaiderit. Koodi sisse olen lisanud mõned eestikeelsed kommentaarid.
+
 {
+    // Dokumendist otsitakse üles slider klassi DIV elemendid (neid on antud veebilehel ainult 1)
     const sliders = document.querySelectorAll(".slider");
     // interval between switching images
     // can't be less than your animation duration in css!
@@ -8,8 +14,11 @@
     const animDuration = 600;
   
     for (let i = 0; i < sliders.length; ++i) {
+      // Kuna veebilehel on ainult üks slaider, siis see tsükkel kestab ainult ühe sammu.
       const slider = sliders[i];
+      // HTML dokumendist leitakse dots klassi DIV
       const dots = slider.querySelector(".dots");
+      // HTML dokumendist leitakse kõik img klassi DIV-id
       const sliderImgs = slider.querySelectorAll(".img");
   
       let currImg = 0;
@@ -19,15 +28,18 @@
   
       // Creates dots and add listeners to them
       for (let i = 0; i < sliderImgs.length; ++i) {
+        // Iga pildi jaoks luuakse üks dot klassi DIV ja see uus DIV seotakse dotClick funktsiooniga, mis käivitub, kui selle DIV peal klõpsatakse hiirega.
         const dot = document.createElement("div");
         dot.classList.add("dot");
         dots.appendChild(dot);
         dot.addEventListener("click", dotClick.bind(null, i), false);
       }
   
+      // HTML dokumendist leitakse kõik vastloodud dot klassi DIV-id ja esimesele neist lisatakse active-dot klass.
       const allDots = dots.querySelectorAll(".dot");
       allDots[0].classList.add("active-dot");
   
+      // Slaideri animatsioon: iga teatud aja tagant (muutuja 'interval') käivitub animateSlider funktsioon, mis vahetab aktiivset pilti.
       sliderImgs[0].style.left = "0";
       timeout = setTimeout(() => {
         animateSlider();
@@ -41,6 +53,7 @@
        * @param {boolean} [right = false] - animate to right
        */
       function animateSlider(nextImg, right) {
+        // See on funktsioon, mida käivitatakse ülevalpool nähtavas koodis iga teatud aja tagant aktiivse pildi vahetamiseks.
         if (!nextImg)
           nextImg = currImg + 1 < sliderImgs.length ? currImg + 2 : 1;
   
@@ -70,6 +83,7 @@
        * @param {number} num - index of clicked dot
        */
       function dotClick(num) {
+        // See on funktsioon, mis kutsutakse välja, kui dot klassi DIV'i peal on hiirega klõpsatud -- funktsioon aktiveerib antud dot DIV'ile vastava pildi.
         if (num == currImg)
           return false;
   
